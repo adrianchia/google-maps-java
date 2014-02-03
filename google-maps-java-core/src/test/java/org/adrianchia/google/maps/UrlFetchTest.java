4954b020f1cd0def294d00f68b28027aba2ebe42
Adrian Chia <achia@adrianchia.com>
2014-02-03 14:26:09 -0600

    WIP...

---
+++ google-maps-java-core/src/test/java/org/adrianchia/google/maps/UrlFetchTest.java
@@ -0,0 +1,54 @@
+/**
+ * Copyright 2014 Adrian Chia
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.adrianchia.google.maps;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.adrianchia.google.maps.timezone.TimeZoneResponse;
+import org.junit.Test;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/**
+ * @author Adrian Chia
+ *
+ */
+public class UrlFetchTest {
+
+    private static final String HOST_NAME = "maps.googleapis.com";
+    private static final String API_PATH = "/maps/api";
+    private static final String TIMEZONE_SERVICE_PATH = "/timezone";
+    private static final String HTTP = "http";
+    private static final String HTTPS = "https";
+    
+    @Test
+    public void testTimeZone() {
+            try {
+                
+                URL url = new URL(HTTPS, HOST_NAME, API_PATH + TIMEZONE_SERVICE_PATH + "/json?location=29.85240200,-95.504595500&timestamp=1391106797&sensor=false");
+                InputStream in = url.openStream();
+                ObjectMapper mapper = new ObjectMapper();
+                TimeZoneResponse tzResponse = mapper.readValue(in, TimeZoneResponse.class);
+                System.out.println(mapper.writeValueAsString(tzResponse));
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        
+    }
+}
